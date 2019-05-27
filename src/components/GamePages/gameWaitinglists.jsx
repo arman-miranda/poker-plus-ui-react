6a195a46aae33d633760859821f5570b97efa44c
marc-fulgar <marc.fulgar@adish.co.jp>
2019-05-27 09:46:21 +0800

    Issue #38: player waitinglists - Adds game waitingslists - Modifies table rendering when no lists are found - react side   - adds gameWaitinglist page   - adds gameWaitinglist page to routes

--- src/components/GamePages/gameWaitinglists.jsx
+++ src/components/GamePages/gameWaitinglists.jsx
@@ -3,11 +3,11 @@ import '../../stylesheets/games_table.css';
 import { Link } from 'react-router-dom';
 import { getDataFromServer } from '../../shared/request_handlers'
 
-class PlayerWaitinglists extends React.Component {
+class GameWaitinglists extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      player_id: this.props.match.params.id,
+      game_id: this.props.match.params.id,
       waitinglists: ""
     }
   }
@@ -18,7 +18,7 @@ class PlayerWaitinglists extends React.Component {
 
   fetchWatingLists() {
     const data = getDataFromServer(
-      `http://localhost:3000/players/${this.state.player_id}/waitinglists`
+      `http://localhost:3000/games/${this.state.game_id}/waitinglists`
     )
     data.then(results => {
       this.setState({ waitinglists: results })
@@ -30,19 +30,10 @@ class PlayerWaitinglists extends React.Component {
       <thead>
         <tr>
           <th>
-            GameID
-          </th>
-          <th>
-            Game Name
+            Player Name
           </th>
           <th>
             Preferred Seat
-          </th>
-          <th>
-            # of Players
-          </th>
-          <th>
-            Status
           </th>
           <th>
           </th>
@@ -51,7 +42,11 @@ class PlayerWaitinglists extends React.Component {
     )
   }
 
-  handleClick(e){
+  handleAcceptClick(e){
+    // todo: ACCEPT LOGIC
+  }
+
+  handleDenyClick(e){
     fetch(`http://localhost:3000/waitinglists/${e.id}`, { method: "DELETE" }).then(
       window.location.reload()
     )
@@ -64,13 +59,13 @@ class PlayerWaitinglists extends React.Component {
         <tbody>
           {waitinglists.map( waitinglist => {
               return (
-                <tr key={waitinglist.game.id}>
-                  <td>{waitinglist.game.id}</td>
-                  <td>{waitinglist.game.name}</td>
+                <tr key={waitinglist.id}>
+                  <td>{waitinglist.player.username}</td>
                   <td>{waitinglist.preferred_seat}</td>
-                  <td>{waitinglist.game.player_games.length} / 9</td>
-                  <td>{waitinglist.game_is_active ? "Ongoing" : "Waiting"}</td>
-                  <td><button onClick={this.handleClick.bind(this, waitinglist)}>Cancel</button></td>
+                  <td>
+                    <button onClick={this.handleAcceptClick.bind(this, waitinglist)}>Accept</button>
+                    <button onClick={this.handleDenyClick.bind(this, waitinglist)}>Deny</button>
+                  </td>
                 </tr>
               )
             })}
@@ -95,4 +90,4 @@ class PlayerWaitinglists extends React.Component {
   }
 }
 
-export default PlayerWaitinglists;
+export default GameWaitinglists;
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import Games from './components/GamePages/games';
 import Game from './components/GamePages/game';
-import PlayerWaitinglists from './components/GamePages/playerWaitinglists'
+import PlayerWaitinglists from './components/GamePages/playerWaitinglists';
+import GameWaitinglists from './components/GamePages/GameWaitinglists'
 
 export const routes = [
   {
@@ -17,5 +18,10 @@ export const routes = [
     exact: true,
     path: '/players/:id/waitinglists',
     component: PlayerWaitinglists
+  },
+  {
+    exact: true,
+    path: '/games/:id/waitinglists',
+    component: GameWaitinglists
   }
 ]
